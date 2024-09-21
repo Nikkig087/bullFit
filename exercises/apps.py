67f053f32fb7e87a30f926550727a01cf63e7cd1
Nicola Getsee <Nicola.getsee@gmail.com>
2024-09-21 17:36:12 +0000

    ran collectstatic for updated files

--- exercises/apps.py
+++ exercises/apps.py
@@ -1,10 +1,4 @@
 from django.apps import AppConfig
-    """
-    Configuration class for the Exercises application.
-
-    This class sets up the configuration for the 'exercises' app, including
-    the default auto field type.
-    """
 
 class ExercisesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
